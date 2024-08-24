510f8874ce7182e26f2318e6425e20f4c281c04b
Karo8870 <cazacuchristian@gmail.com>
2024-08-25 00:30:13 +0300

    Added files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import { motion, useScroll, useTransform } from 'framer-motion';
+
 export default function Home() {
-  return <></>;
+  const { scrollYProgress } = useScroll();
+
+  const scaleY = useTransform(scrollYProgress, [0, 1], [1, 10]);
+  const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
+
+  return (
+    <main className='bg-black'>
+      <section className='flex flex-col items-center h-[300vh]'>
+        <motion.h1
+          className='text-white text-6xl py-80 sticky top-4'
+          style={{ opacity }}
+        >
+          Schimbări climatice
+        </motion.h1>
+        <motion.div
+          className='bg-rose-800 w-2/3 h-40 sticky top-4 origin-top'
+          style={{ scaleY }}
+        ></motion.div>
+      </section>
+    </main>
+  );
 }
